dcba415bc3c1d98815ac4a4a87bdbb58aa4207c1
Nyakallo1996 <Lekgoabamahlaks@gmail.com>
2024-05-05 21:40:38 +0200

    react

--- Chapter_4/src/App.js
+++ Chapter_4/src/App.js
@@ -6,19 +6,9 @@ import Rating from "./Rating";
 class App extends Component {
   render() {
     return (
-      <div>
-        <h1>
-          My first react app!
-          <Products />
-          <Button variant="outline-danger">Default</Button>
-          <div>
-            <Rating rating="1" />
-            <Rating rating="2" />
-            <Rating rating="3" />
-            <Rating rating="4" />
-            <Rating rating="5" />
-          </div>
-        </h1>
+      <div className="App">
+        <Products />
+        <Button variant="outline-danger">Default</Button>
       </div>
     );
   }

6b70db58da94d1e9cffc0d0367484f1650a30f0b
Nyakallo1996 <Lekgoabamahlaks@gmail.com>
2024-05-06 09:41:55 +0200

    react

---
+++ Chapter_5/src/App.js
@@ -0,0 +1,19 @@
+import React, { Component } from "react";
+import JumboTronComponent from "./JumboTronComponent";
+
+class App extends Component {
+  render() {
+    return (
+      <div className="App">
+        <JumboTronComponent>
+        This is a long sentence, and I want to insert content into the  
+        jumbotron component from the outside.
+        </JumboTronComponent>
+        
+        
+      </div>
+    );
+  }
+}
+
+export default App;

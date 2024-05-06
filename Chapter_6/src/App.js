5bdef6bc3d13a0a0d9d61080adf488efad586c1c
Nyakallo1996 <Lekgoabamahlaks@gmail.com>
2024-05-06 22:00:30 +0200

    react

--- Chapter_6/src/App.js
+++ Chapter_6/src/App.js
@@ -1,16 +1,12 @@
 import React, { Component } from "react";
 import JumboTronComponent from "./JumboTronComponent";
+import UserForm from "./UserForm";
 
 class App extends Component {
   render() {
     return (
       <div className="App">
-        <JumboTronComponent>
-        This is a long sentence, and I want to insert content into the  
-        jumbotron component from the outside.
-        </JumboTronComponent>
-        
-        
+        <UserForm/>
       </div>
     );
   }

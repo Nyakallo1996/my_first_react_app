043cc92d650dd851f912fdbd6cbd6fd5788ac64c
Nyakallo1996 <Lekgoabamahlaks@gmail.com>
2024-05-06 22:06:34 +0200

    react

---
+++ Chapter_7/src/App.js
@@ -0,0 +1,15 @@
+import React, { Component } from "react";
+import JumboTronComponent from "./JumboTronComponent";
+import UserForm from "./UserForm";
+
+class App extends Component {
+  render() {
+    return (
+      <div className="App">
+        <UserForm/>
+      </div>
+    );
+  }
+}
+
+export default App;
---
+++ Chapter_7/src/Product.js
@@ -0,0 +1,29 @@
+import React, {Component} from "react";
+import Rating from "./Rating";
+import { Media } from 'react-bootstrap';
+
+class Product extends Component {
+    constructor(props) {
+        super(props);
+    }
+    render() {
+        return (
+            <div>
+                <media>
+                    <img width={64} height={64} className="mr-3" src={this.props.data.imageUrl} alt="Image" />
+                    <Media.Body>
+                        <h5>{this.props.data.productName}</h5>
+                        { this.props.data.releasedDate }
+                        <Rating 
+                           rating={ this.props.data.rating }
+                           numOfReviews={this.props.data.numOfReviews}
+                        />
+                        <p>{this.props.data.description}</p>
+                    </Media.Body>
+                </media>
+            </div>
+        );
+    }
+}
+
+export default Product;

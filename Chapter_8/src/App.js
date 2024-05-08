19446408d5ba0c80f1915b4e3438e6185484fed6
Nyakallo1996 <Lekgoabamahlaks@gmail.com>
2024-05-08 08:42:57 +0200

    react

--- Chapter_8/src/App.js
+++ Chapter_8/src/App.js
@@ -2,15 +2,49 @@ import React, { Component } from "react";
 import JumboTronComponent from "./JumboTronComponent";
 import UserForm from "./UserForm";
 import GitHub from "./GitHub";
+import {BrowserRouter, Router, Switch} from "react-router-dom";
 
 class App extends Component {
   render() {
     return (
       <div className="App">
-        <GitHub/>
+        <Header/>
       </div>
     );
   }
 }
+
+class Header extends Component { 
+  render(){ 
+    return ( 
+        <BrowserRouter> 
+          <div>                    
+            <Switch>               
+              <Route path="/github" component={GitHub} />               
+              <Route exact path="/" component={Home} />              
+              <Route path="/*" component={NotFound} />                        
+            </Switch>    
+          </div>   
+        </BrowserRouter>               
+    ) 
+  } 
+}
+
+class Home extends Component { 
+  render(){ 
+    return ( 
+      <div> 
+        Home         
+      </div> 
+    ) 
+  } 
+} 
+ 
+class NotFound extends Component {
+  render() {
+    return <div>Not Found</div>
+  }
+} 
+ 
 
 export default App;

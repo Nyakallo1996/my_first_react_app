7872a52b3ecbb8d2dba18f1f4e568f1876207b8b
Nyakallo1996 <Lekgoabamahlaks@gmail.com>
2024-05-09 08:08:19 +0200

    react

--- Chapter_9/reactcrud/src/App.js
+++ Chapter_9/reactcrud/src/App.js
@@ -4,6 +4,7 @@ import React, { Component } from "react";
 import firebase from "firebase/compat/app";
 import "firebase/compat/auth";
 import "firebase/compat/firestore";
+import User from "./User";
 
 class App extends Component {
   constructor() {
@@ -13,7 +14,7 @@ class App extends Component {
   render() {
     return (
       <div>
-        <h1>Check Console</h1>
+        <User />
       </div>
     );
   }

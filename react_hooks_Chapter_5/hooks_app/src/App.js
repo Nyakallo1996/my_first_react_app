829c639edb3dec1fe9eeb36903173a84d06e86c6
Nyakallo1996 <Lekgoabamahlaks@gmail.com>
2024-05-17 12:11:58 +0200

    react

---
+++ react_hooks_Chapter_5/hooks_app/src/App.js
@@ -0,0 +1,28 @@
+import React from "react";
+import Products from "./Products";
+import "bootstrap/dist/css/bootstrap.min.css";
+import { Button } from "react-bootstrap";
+import Rating from "./Rating";
+
+const formatName = (user) => {
+  return user.firstName + " " + user.lastName;
+};
+
+const App = () => {
+  const user = {
+    firstName: "Greg",
+    lastName: "Lim",
+  };
+
+  return (
+    <div className="App">
+      <h1>Hello, {formatName(user)}</h1>
+      <Products />
+      <Button variant="primary" disabled>
+        Default
+      </Button>
+    </div>
+  );
+};
+
+export default App;

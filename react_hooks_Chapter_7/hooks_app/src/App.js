2e34ae0bcdb09d059dbaacf040d67c2cc6685e67
Nyakallo1996 <Lekgoabamahlaks@gmail.com>
2024-05-18 23:15:31 +0200

    react

--- react_hooks_Chapter_7/hooks_app/src/App.js
+++ react_hooks_Chapter_7/hooks_app/src/App.js
@@ -1,24 +1,13 @@
 import React from "react";
-import Products from "./Products";
 import "bootstrap/dist/css/bootstrap.min.css";
-import { Button } from "react-bootstrap";
-import Rating from "./Rating";
-import JumbotronComponent from "./JumbotronComponent";
-import UserForm from "./UserForm";
-
-const formatName = (user) => {
-  return user.firstName + " " + user.lastName;
-};
+import GitHub from "./GitHub";
 
 const App = () => {
-  const user = {
-    firstName: "Greg",
-    lastName: "Lim",
-  };
+  
 
   return (
     <div className="App">
-      <UserForm />
+      <GitHub />
       
     </div>
   );
--- react_hooks_Chapter_7/hooks_app/src/GitHub.js
+++ react_hooks_Chapter_7/hooks_app/src/GitHub.js
@@ -1,1 +1,48 @@
-import React, {useEffect} from "react";
+import React, {useEffect, useState} from "react";
+import axios from "axios";
+import ReactLoading from "react-loading";
+import { Media } from 'react-bootstrap';
+
+function GitHub() {
+    
+    const [data, setData] = useState([]);
+    const [searchTerm, setSearchTerm] = useState("greg");
+    const [isLoading, setIsLoading] = useState(true);
+    
+    useEffect(() => {
+        axios.get(`https://api.github.com/search/users?q=${searchTerm}`)
+             .then(res => {
+                setData(res.data.items)
+                setIsLoading(false);
+             });
+    },[])
+
+    const listUsers = data.map((user) =>              
+        <Media key={user.id}> 
+            <a href={user.html_url}>     
+                <img 
+                    width={64} 
+                    height={64} 
+                    className="mr-3" 
+                    src={user.avatar_url} 
+                    alt="Generic placeholder" 
+                /> 
+            </a> 
+            <Media.Body> 
+                <h5>Login: {user.login}</h5> 
+                <p>Id: { user.id }</p> 
+            </Media.Body> 
+        </Media>   
+    );
+
+    return (
+        <div>
+            <h3>GitHub Users Results</h3>
+            {isLoading && 
+            <ReactLoading type="spinningBubbles" color="#444"/>}
+            {listUsers}
+        </div>
+    );
+}
+
+export default GitHub;

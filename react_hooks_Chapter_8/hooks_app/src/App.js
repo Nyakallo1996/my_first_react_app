c9709b2f0c1206792a08ab3f0099c2f0ed35e142
Nyakallo1996 <Lekgoabamahlaks@gmail.com>
2024-05-19 12:41:15 +0200

    react

---
+++ react_hooks_Chapter_8/hooks_app/src/App.js
@@ -0,0 +1,16 @@
+import React from "react";
+import "bootstrap/dist/css/bootstrap.min.css";
+import GitHub from "./GitHub";
+
+const App = () => {
+  
+
+  return (
+    <div className="App">
+      <GitHub />
+      
+    </div>
+  );
+};
+
+export default App;

4d04ca680a16a6cad46c18dc7244463c7f829607
Nyakallo1996 <Lekgoabamahlaks@gmail.com>
2024-05-20 22:13:01 +0200

    react

--- Tutorial_One_Dynamic_Table/dynamic/src/TableData.js
+++ Tutorial_One_Dynamic_Table/dynamic/src/TableData.js
@@ -4,6 +4,7 @@ const TableData = [
     {id:3, fullName:"Monika Singh", age:18, city:"New Delhi"},
     {id:4, fullName:"Sunil Kumar", age:22, city:"Jaipur"},
     {id:5, fullName:"Kajol Kumari", age:21, city:"Chennai"}
+    
 ]
 
 export default TableData;

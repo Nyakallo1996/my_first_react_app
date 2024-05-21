fed67bba78948e125a3d077f9ca9d4e1d2199057
Nyakallo1996 <Lekgoabamahlaks@gmail.com>
2024-05-21 21:11:15 +0200

    react

---
+++ Tutorial_Four_Portfolio_Gallery/portfolio/src/App.js
@@ -0,0 +1,11 @@
+import "../node_modules/bootstrap/dist/css/bootstrap.min.css";
+import Gallery from "./Gallery";
+
+function App() {
+  return (
+    <div>
+      <Gallery/>
+    </div>
+  )
+}
+export default App;

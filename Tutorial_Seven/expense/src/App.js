ac1086abec10818f2a98026911fe48ced4f864d1
Nyakallo1996 <Lekgoabamahlaks@gmail.com>
2024-05-31 11:53:40 +0200

    react

--- Tutorial_Seven/expense/src/App.js
+++ Tutorial_Seven/expense/src/App.js
@@ -1,9 +1,17 @@
-
+import React from "react";
+import {BrowserRouter as Router, Route, Routes} from "react-router-dom";
+import { Auth } from "./pages/auth";
+import { ExpenseTracker } from "./pages/expense-tracker";
 
 function App() {
   return (
     <div className="App">
-      
+      <Router>
+        <Routes>
+          <Route path="/" exact element={<Auth/>} />
+          <Route path="/expense-tracker"  element={<ExpenseTracker/>} />
+        </Routes>
+      </Router>
     </div>
   );
 }

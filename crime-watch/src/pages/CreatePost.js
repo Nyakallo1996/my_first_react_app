4e474596ab607be27c7f0599fa19723ead41b498
Nyakallo1996 <Lekgoabamahlaks@gmail.com>
2024-06-09 13:59:01 +0200

    React

--- crime-watch/src/pages/CreatePost.js
+++ crime-watch/src/pages/CreatePost.js
@@ -1,20 +1,33 @@
 import React, {useState} from "react";
 import { addDoc, collection } from "firebase/firestore";
-import { db, auth } from "../firebase-config";
+import { ref, uploadBytes, getDownloadURL } from "firebase/storage";
+import { db, auth, storage } from "../firebase-config";
 import { useNavigate } from "react-router-dom";
 
 function CreatePost() {
     
     const [title, setTitle] = useState("");
     const [postText, setPostText] = useState("");
+    const [image, setImage] = useState(null);
 
     const postsCollectionRef = collection(db, "posts");
     let navigate = useNavigate();
 
+    const uploadImage = async (imageFile) => {
+        if (!imageFile) return null;
+
+        const imageRef = ref(storage, `images/${imageFile.name + new Date().getTime()}`);
+        await uploadBytes(imageRef, imageFile);
+        const imageUrl = await getDownloadURL(imageRef);
+        return imageUrl;
+    };
+
     const createPost = async () => {
+        const imageUrl = await uploadImage(image);
         await addDoc(postsCollectionRef, {
             title,
             postText,
+            imageUrl,
             author: {name: auth.currentUser.displayName, id: auth.currentUser.uid}
         });
         navigate("/");
@@ -34,6 +47,16 @@ function CreatePost() {
                 <textarea placeholder="Post.." onChange={(event) => {
                     setPostText(event.target.value);
                 }}/>
+            </div>
+            <div className="inputGp">
+                    <label>Image:</label>
+                    <input
+                        type="file"
+                        accept="image/*"
+                        onChange={(event) => {
+                            setImage(event.target.files[0]);
+                        }}
+                    />
             </div>
             <button onClick={createPost}>Submit Post</button>
         </div>
